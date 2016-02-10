45eb60460b4b4f8b5aca65468999a8985165e66f
UdjinM6 <UdjinM6@users.noreply.github.com>
2016-02-11 01:19:30 +0300

    Show 2 decimals for block difficulty and size

--- src/block.js
+++ src/block.js
@@ -7,13 +7,13 @@ function Block(height, numTransactions, /*outputTotal*/difficulty, blockSize) {
 		Floatable.call(this);
 
 		// var outputBTC = Math.floor(outputTotal / satoshi) + " BTC";
-		var blockSizeKB = Math.floor(blockSize / 1024) + " KB";
+		var blockSizeKB = (blockSize / 1024).toFixed(2) + " KB";
 
 		this.width = this.height = 500;
 
 		this.addImage(blockImage, this.width, this.height);
 		// this.addText("Block #" + height + "<br />Number of Transactions: " + numTransactions + "<br />Transaction Volume: " + outputBTC + "<br />Block Size: " + blockSizeKB);
-		this.addText("Block #" + height + "<br />Number of Transactions: " + numTransactions + "<br />Block Difficulty: " + difficulty + "<br />Block Size: " + blockSizeKB);
+		this.addText("Block #" + height + "<br />Number of Transactions: " + numTransactions + "<br />Block Difficulty: " + difficulty.toFixed(2) + "<br />Block Size: " + blockSizeKB);
 		this.initPosition();
 	}
 	
